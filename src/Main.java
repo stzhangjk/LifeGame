cb85d4ff2047d9b746bbb4eaa237ca8e88b1d3e3
Grady0000 <stzhangjk@163.com>
2016-08-05 08:43:05 +0800

    完成

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,10 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args){
-
-
-
         SwingUtilities.invokeLater(() -> {
             MainFrame mainFrame = new MainFrame();
-
             mainFrame.setExtendedState(Frame.MAXIMIZED_BOTH);
             mainFrame.setVisible(true);
         });
-
-
     }
 }
